854d2840d642ed803a93f350632798b680037437
CalmTheBlues <mrichter7102@gmail.com>
2024-11-04 18:09:58 -0500

    need to make main function

--- blackjack_simulator.py
+++ blackjack_simulator.py
@@ -13,7 +13,18 @@ class BlackJack:
         self.wager = None
         self.player_hands = []
         self.num_bots = 0
-    
+        self.card_count = 52 * int(num_decks)
+        self.max_deck = 52 * int(num_decks)
+
+    def getNewDecks(self, num_decks):
+        self.deck_url = deck_url_base + num_decks
+        response = requests.get(self.deck_url)
+        self.deck = response.json()
+        self.deck_id = self.deck["deck_id"]
+        self.card_count = 52 * int(num_decks)
+        self.max_deck = 52 * int(num_decks)
+
+
     def playmates(self, numbots):
         self.num_bots = numbots
         self.player_hands = [[] for _ in range(numbots + 2)]  # empty lists for each hand plus 2 is for  yourself and dealer
@@ -26,10 +37,20 @@ class BlackJack:
                 card = requests.get(draw_url)
                 drawn_card = card.json()["cards"][0]  # get the first card from the response
                 self.player_hands[j].append(drawn_card)  # append the card to the player's hand
+                self.card_count -= 1
+    def hit(self, idx):
+        draw_url = f"https://deckofcardsapi.com/api/deck/{self.deck_id}/draw/?count={1}"
+        card = requests.get(draw_url)
+        drawn_card = card.json()["cards"][0]
+        self.player_hands[idx].append(drawn_card)
+        self.card_count -= 1
     
-    def printID(self):
-        print(self.deck_id)
-    
+    def deckSize(self):
+        if self.card_count <= (0.25 * self.max_deck):
+            print("time to reshuffle")
+
+
+
     def printHands(self):
         for idx, hand in enumerate(self.player_hands):
             if idx == 0:
@@ -45,10 +66,15 @@ class BlackJack:
 
     
 if __name__ == "__main__":
-    # print("How many decks would you like to play with (1-6)")
-    num_decks = input("How many decks would you like to play with (1-6): ")
-    game = BlackJack(num_decks)
-    numBots = int(input("How man bots would you like to play with: "))
-    game.playmates(numBots)
-    game.printHands()
+    while (True):
+        num_decks = input("How many decks would you like to play with (1-6): ")
+            if num_decks == 'e':
+                break
+        
+        game = BlackJack(num_decks)
+        numBots = int(input("How man bots would you like to play with: "))
+        game.playmates(numBots)
+        game.printHands()
+        game.hit(0)
+        game.printHands()
     
